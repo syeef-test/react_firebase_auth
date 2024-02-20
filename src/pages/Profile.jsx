d72678560c2fa37ae1ea5a0e53d61d9f0ce88f1a
syeef <syeefislam@yahoo.com>
2024-02-20 08:01:11 +0530

    password changed succesfully

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,79 @@
-import React, { useContext } from "react";
+import React, { useRef, useState, useContext } from "react";
+import AuthContext from "../store/auth-context";
+import { useHistory } from "react-router-dom";
 
 function Profile() {
+  const passwordRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+
+  const authCtx = useContext(AuthContext);
+
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const password = passwordRef.current.value;
+
+    const key = import.meta.env.VITE_FIREBASE_APP_ID;
+
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${key}`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            idToken: authCtx.token,
+            password: password,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error.message);
+      } else {
+        const data = await response.json();
+        console.log(data.idToken);
+
+        // authCtx.login(data.idToken);
+        // history.push("/profile");
+      }
+
+      console.log("Password changed successfully!");
+    } catch (error) {
+      console.error("Error in password change:", error.message);
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+
+    passwordRef.current.value = "";
+  };
+
   return (
-    <>
-      <h2>Profile Page</h2>
-    </>
+    <div>
+      <h2>Profile</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            minLength="7"
+            id="password"
+            ref={passwordRef}
+            required
+          />
+        </div>
+        <button type="submit" disabled={loading}>
+          {loading ? "Reseting Password..." : "Reset"}
+        </button>
+      </form>
+    </div>
   );
 }
 
